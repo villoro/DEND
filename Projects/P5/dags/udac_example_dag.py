2908d09d5c1ff745eec32a96762339045b17f6f8
Villoro <villoro7@gmail.com>
2020-04-13 12:40:42 +0200

    [NEW] project DAG + sql queries

--- Projects/P5/dags/udac_example_dag.py
+++ Projects/P5/dags/udac_example_dag.py
@@ -2,64 +2,103 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
+from airflow.operators import (
+    StageToRedshiftOperator,
+    LoadFactOperator,
+    LoadDimensionOperator,
+    DataQualityOperator,
+)
 from helpers import SqlQueries
 
-# AWS_KEY = os.environ.get('AWS_KEY')
-# AWS_SECRET = os.environ.get('AWS_SECRET')
 
-default_args = {
-    'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
-}
+default_args = {"owner": "udacity", "start_date": datetime(2019, 1, 12)}
 
-dag = DAG('udac_example_dag',
-          default_args=default_args,
-          description='Load and transform data in Redshift with Airflow',
-          schedule_interval='0 * * * *'
-        )
+dag = DAG(
+    "udac_example_dag",
+    default_args=default_args,
+    description="Load and transform data in Redshift with Airflow",
+    schedule_interval="0 * * * *",
+)
 
-start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+start_operator = DummyOperator(task_id="Begin_execution", dag=dag)
 
 stage_events_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_events',
-    dag=dag
+    task_id="Stage_events", dag=dag, table="staging_events", path_s3=None  # TODO
 )
 
 stage_songs_to_redshift = StageToRedshiftOperator(
-    task_id='Stage_songs',
-    dag=dag
+    task_id="Stage_songs", dag=dag, table="staging_songs", path_s3=None  # TODO
 )
 
 load_songplays_table = LoadFactOperator(
-    task_id='Load_songplays_fact_table',
-    dag=dag
+    task_id="Load_songplays_fact_table",
+    dag=dag,
+    table="songplays",
+    query_select=SqlQueries.songplay_table_insert,
 )
 
 load_user_dimension_table = LoadDimensionOperator(
-    task_id='Load_user_dim_table',
-    dag=dag
+    task_id="Load_user_dimension_table",
+    dag=dag,
+    table="users",
+    query_select=SqlQueries.user_table_insert,
 )
 
 load_song_dimension_table = LoadDimensionOperator(
-    task_id='Load_song_dim_table',
-    dag=dag
+    task_id="Load_song_dimension_table",
+    dag=dag,
+    table="songs",
+    query_select=SqlQueries.song_table_insert,
 )
 
 load_artist_dimension_table = LoadDimensionOperator(
-    task_id='Load_artist_dim_table',
-    dag=dag
+    task_id="Load_artist_dimension_table",
+    dag=dag,
+    table="artists",
+    query_select=SqlQueries.artist_table_insert,
 )
 
 load_time_dimension_table = LoadDimensionOperator(
-    task_id='Load_time_dim_table',
-    dag=dag
+    task_id="Load_time_dimension_table",
+    dag=dag,
+    table="time",
+    query_select=SqlQueries.time_table_insert,
 )
 
 run_quality_checks = DataQualityOperator(
-    task_id='Run_data_quality_checks',
-    dag=dag
+    task_id="Run_data_quality_checks",
+    dag=dag,
+    dict_checks={
+        "songplays": ["songplay_id", "sessionid", "start_time"],
+        "users": ["userid"],
+        "songs": ["song_id"],
+        "artists": ["artist_id"],
+        "time": ["start_time"],
+    },
 )
 
-end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+end_operator = DummyOperator(task_id="Stop_execution", dag=dag)
+
+# Stagging tables
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+# Fact table
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+# Dim tables
+stage_events_to_redshift >> load_user_dimension_table
+stage_songs_to_redshift >> load_song_dimension_table
+stage_songs_to_redshift >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+# Data quality
+load_songplays_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_song_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+# Dummy end
+run_quality_checks >> end_operator
