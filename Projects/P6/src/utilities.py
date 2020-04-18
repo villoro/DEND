120606af7e57d6a6b3c7bbc5455162a6ae81d990
Villoro <villoro7@gmail.com>
2020-04-18 12:26:11 +0200

    [NEW] rapidapi code for P6 + utilities

---
+++ Projects/P6/src/utilities.py
@@ -0,0 +1,22 @@
+"""
+    Log utilities using loguru
+"""
+
+import sys
+import configparser
+from datetime import date
+
+from loguru import logger as log
+
+
+config = configparser.ConfigParser()
+config.read("../config.cfg")
+
+LOG_CONFIG = {
+    "handlers": [
+        {"sink": sys.stdout, "level": "DEBUG"},
+        {"sink": f"../logs/{date.today():%Y_%m_%d}.log", "level": "INFO"},
+    ]
+}
+log.configure(**LOG_CONFIG)
+log.enable("vflights")
